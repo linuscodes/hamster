fccc11d8a88da15a3b0e520c5fadfa3c86425b03
Linus Rogge <moin@linuscodes.com>
2020-09-02 08:15:13 +0200

    Add nutzliche prozduren

---
+++ nuetzliche_prozeduren.java
@@ -0,0 +1,39 @@
+void main() {
+
+}
+
+void sicheresVor() {
+   if(vornFrei()) {
+      vor();
+   }
+}
+
+void sicheresNimm() {
+   if(kornDa()) {
+      nimm();
+   }
+}
+
+void sicheresGib() {
+   if(!maulLeer()) {
+      gib();
+   }
+}
+
+void laufeVor() {
+   while(vornFrei()) {
+      vor();
+   }
+}
+
+void sammle() {
+   while(kornDa()) {
+      nimm();
+   }
+}
+
+void legAb() {
+   while(!maulLeer()) {
+      gib();
+   }
+}
